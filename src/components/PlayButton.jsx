0ed527c22e869aace72651ce4adc718359c52f09
badr-mohyuddin <mofi3a@live.com.au>
2025-11-04 12:41:19 +1030

    Implemented differerent components for features in the starter code

---
+++ src/components/PlayButton.jsx
@@ -0,0 +1,11 @@
+function PlayButton() {
+    return (
+    <>
+    <div className="btn-group" role="group" aria-label="Play Controls">
+        <button id="play" className="btn btn-outline-primary">Play</button>
+        <button id="stop" className="btn btn-outline-primary">Stop</button>
+    </div>
+    </>
+    );
+}
+export default PlayButton;
